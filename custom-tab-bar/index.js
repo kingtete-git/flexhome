6fc5713eb0cd9a0f40bc7a2637f206d70fab29c1
Wang Teng <396294005@qq.com>
2021-07-16 00:24:31 +0800

    diyi
    diyi

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -4,12 +4,12 @@ Component({
     color: "#7A7E83",
     selectedColor: "#3cc51f",
     list: [{
-      pagePath: "/index/index",
+      pagePath: "/index/index/index",
       iconPath: "/image/icon_component.png",
       selectedIconPath: "/image/icon_component_HL.png",
       text: "组件"
     }, {
-      pagePath: "/index/index2",
+      pagePath: "/index/index2/index2",
       iconPath: "/image/icon_API.png",
       selectedIconPath: "/image/icon_API_HL.png",
       text: "接口"
